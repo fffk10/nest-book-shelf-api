78ca81e6fc71bc617d2e0a432bc6c4483be9bd72
fffk10 <warafffk10@gmail.com>
2024-03-17 01:14:33 +0900

    テスト形だけ

--- src/books/books.service.spec.ts
+++ src/books/books.service.spec.ts
@@ -15,4 +15,28 @@ describe('BooksService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  // it('findByAll success.', async () => {
+  //   // ==== expected ==== //
+  //   const expected = [
+  //     {
+  //       id: 1,
+  //       title: 'Book 1',
+  //       author: 'Author 1',
+  //       isbn: '978-1234567890',
+  //     },
+  //     {
+  //       id: 2,
+  //       title: 'Book 2',
+  //       author: 'Author 2',
+  //       isbn: '978-0987654321',
+  //     },
+  //   ];
+
+  //   // ==== exec ==== //
+  //   const result = await service.findAll();
+
+  //   // ==== assert ==== //
+  //   expect(result).toBe(expected);
+  // });
 });
