7a8c72ab8ef23955f4d9471a859e78feac27685e
fffk10 <warafffk10@gmail.com>
2024-03-19 00:29:44 +0900

    jest下準備

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  clearMocks: true,
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ['<rootDir>/singleton.ts'],
+  moduleFileExtensions: [
+    'js',
+    'mjs',
+    'cjs',
+    'jsx',
+    'ts',
+    'tsx',
+    'json',
+    'node',
+    'mts',
+  ],
+};
--- src/books/books.service.spec.ts
+++ src/books/books.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BooksService } from 'src/books/books.service';
+import { prismaMock } from 'singleton';
 
 describe('BooksService', () => {
   let service: BooksService;
@@ -16,27 +17,56 @@ describe('BooksService', () => {
     expect(service).toBeDefined();
   });
 
-  // it('findByAll success.', async () => {
-  //   // ==== expected ==== //
-  //   const expected = [
-  //     {
-  //       id: 1,
-  //       title: 'Book 1',
-  //       author: 'Author 1',
-  //       isbn: '978-1234567890',
-  //     },
-  //     {
-  //       id: 2,
-  //       title: 'Book 2',
-  //       author: 'Author 2',
-  //       isbn: '978-0987654321',
-  //     },
-  //   ];
+  it('findByAll success.', async () => {
+    // ==== expected ==== //
+    const expected = [
+      {
+        id: 1,
+        title: 'Book 1',
+        author: 'Author 1',
+        isbn: '978-1234567890',
+        publisher: 'Publisher 1',
+        publishedDate: '2022-01-01',
+        description: 'Description 1',
+        coverImage: 'cover1.jpg',
+      },
+      {
+        id: 2,
+        title: 'Book 2',
+        author: 'Author 2',
+        isbn: '978-0987654321',
+        publisher: 'Publisher 2',
+        publishedDate: '2021-06-15',
+        description: 'Description 2',
+        coverImage: 'cover2.jpg',
+      },
+    ];
 
-  //   // ==== exec ==== //
-  //   const result = await service.findAll();
+    // ==== mock ==== //
+    prismaMock.book.findMany.mockResolvedValue(expected);
 
-  //   // ==== assert ==== //
-  //   expect(result).toBe(expected);
-  // });
+    // ==== assert ==== //
+    await expect(service.findAll()).resolves.toEqual([
+      {
+        id: 1,
+        title: 'Book 1',
+        author: 'Author 1',
+        isbn: '978-1234567890',
+        publisher: 'Publisher 1',
+        publishedDate: '2022-01-01',
+        description: 'Description 1',
+        coverImage: 'cover1.jpg',
+      },
+      {
+        id: 2,
+        title: 'Book 2',
+        author: 'Author 2',
+        isbn: '978-0987654321',
+        publisher: 'Publisher 2',
+        publishedDate: '2021-06-15',
+        description: 'Description 2',
+        coverImage: 'cover2.jpg',
+      },
+    ]);
+  });
 });
