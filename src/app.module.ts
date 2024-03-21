be8756e7089e47f8e6f6728709376e574ce0ac46
fffk10 <warafffk10@gmail.com>
2024-03-21 21:35:26 +0900

    NestConfig導入

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from 'src/app.controller';
 import { AppService } from 'src/app.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { BooksModule } from 'src/books/books.module';
 
 @Module({
-  imports: [BooksModule],
+  imports: [
+    BooksModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env.${process.env.node_env}'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
